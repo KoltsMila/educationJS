1290b0258b70fffa932a252761466ee04d4d2176
Mila Koltsova <133654525+KoltsMila@users.noreply.github.com>
2023-06-01 08:18:54 +0300

    Ввод строки побуквенно, поиск сайта и рандомное хождение по его ссылкам
    проблемы:
    1. залипает ввод текста в поисковую строку - некоторые буквы пропускаются
    2. нет поиска по другим странницам, из-за отсутствия названия имени и id у кнопки "далее".

--- BingBot.js
+++ BingBot.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         BingBot
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       Koltsova Mila
 // @match        https://www.bing.com/*
+// @match        https://travelest.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
@@ -13,21 +14,60 @@ let keywords = ["Пеший путеводитель по Афинам - гор�
 let keyword = keywords[getRandom(0, keywords.length)];
 let links = document.links;
 let search = document.getElementsByName("search")[0];
+let bingInput = document.getElementById("sb_form_q");
+//let bingInput = document.getElementsByName("q")[0];
+
 
 if (search !== undefined) {
-  document.getElementById("sb_form_q").value = keyword;
-  search.click();
+  let i = 0;
+  let timerId = setInterval(() => {
+    bingInput.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      search.click();
+    }
+  }, 300);
+} else if (location.hostname == "travelest.ru") {
+  console.log("Мы на целевом сайте");
+  setInterval(() => {
+    let index = getRandom(0, links.length);
+    if (getRandom(0, 101) >= 70) {
+      location.href = "https://www.bing.com/";
+    }
+    if(links[index].href.indexOf("travelest.ru") != -1) links[index].click();
+  }, getRandom(2000, 5000))
 } else {
-
+  let nextBingPage = true;
   for(let i = 0; i < links.length; i++) {
     if(links[i].href.indexOf("travelest.ru") != -1) {
       let link = links[i];
+      nextBingPage = false;
       console.log("Нашел строку " + link);
-      link.click();
+      setTimeout(() => {
+        link.click();
+      }, getRandom(2500, 5000))
       break;
     }
   }
-}
+  let elementExist = setInterval(() => {
+    let element = document.querySelector(".sb_pagS sb_pagS_bp b_widePag sb_bp");
+    if (element != null) {
+      if (element.innerText == "5") {
+        nextBingPage = false;
+        location.href = "https://www.bing.com/";
+      }
+      clearInterval(elementExist)
+    }
+  }, 100)
+  }
+
+//if (nextBingPage) {
+//setTimeout(() => {
+//__.click();
+//}, getRandom(3000, 8000));
+//}
+//}
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min) + min)
